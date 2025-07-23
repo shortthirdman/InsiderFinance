cf85661f4f2f403f6bdf4c75c6434469a811b13b
Swetank Mohanty <swetank.mohanty@outlook.com>
2025-07-23 16:36:31 +0530

    core(streamlit): Updated configuration with new page

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,20 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
+# if "visibility" not in st.session_state:
+#     st.session_state.visibility = "visible"
+#     st.session_state.disabled = False
 
 # Set up the Streamlit app
 st.title("Insider Finance: Stock Portfolio Optimization with Various Strategies")
 
 st.write("Hello Streamlit-er 👋")
 
-trading_strategy_sharpe = st.Page(page="pages/1_TradingStrategy_SharpeRatio.py", title="Trading Strategy - Sharpe Ratio", icon=":streamlit:")
-stock_portfolio_deap = st.Page(page="pages/2_StockPortfolio_DEAP.py", title="Stock Portfolio Optimization with DEAP", icon=":streamlit:")
+trading_strategy_sharpe = st.Page(page="pages/1_TradingStrategy_SharpeRatio.py", title="Trading Strategy - Sharpe Ratio")
+stock_portfolio_deap = st.Page(page="pages/2_StockPortfolio_DEAP.py", title="Stock Portfolio Optimization with DEAP")
+bitcoin_rate_of_change = st.Page(page="pages/3_Optimize_Bitcoin_RateofChange.py", title="Optimize Bitcoin Rate of Change")
 
-
-pg = st.navigation([trading_strategy_sharpe, stock_portfolio_deap], title="Insider Finance", icon=":streamlit:")
+## title="Insider Finance", icon=":streamlit:"
+pg = st.navigation([trading_strategy_sharpe, stock_portfolio_deap, bitcoin_rate_of_change])
 
 pg.run()
